9caf62b01542d25c3011548808923c3f4cf45062
carolin santoro <carolin_santoro@yahoo.com.br>
2019-08-21 15:35:03 -0300

    concluded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Number.number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  5.times do
+    Review.create(
+    content: Faker::Restaurant.review,
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    restaurant_id: restaurant.id
+  )
+  end
+end
